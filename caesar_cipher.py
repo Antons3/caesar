e5e11fc90a9b3711287d4f19528f94881b875a0c
Antons3 <149572546+Antons3@users.noreply.github.com>
2023-11-15 13:21:11 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,38 +1,44 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
+
+def answer1(text, rotation):  #использует 1 режим
+  result = ""
+  for letter in text:  #берет letter из text
+    print(letter)
+    if (alphabet.find(letter) == -1):  #проверяет есть ли буква в алфавите
+      result += letter
+    else:  #прибавляет введенное количсетво символов к каждому символу в тексте
+      result += (alphabet[(alphabet.find(letter) + rotation) % len(alphabet)])
+  return result
+
+
+def answer2(text, rotation):  #использует 2 режим
+
+  result = ""
+  for letter in text:
+    if (alphabet.find(letter) == -1):
+      result += letter
     else:
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
+      result += (alphabet[(alphabet.find(letter) - rotation) % len(alphabet)])
+  return result
 
-def d(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
 
-w = """
+mode = """
 1. Encrypt text
 2. Decrypt text
 3. Bruteforce all rotations
 Choose mode: """
-mode = int(input(w))
+mode = int(input(mode))
 
-if mode == 1:
+if mode == 1:  # запуск 1 режима
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
-elif mode == 2:
+  print("Encrypted: " + answer1(text, rotation))
+elif mode == 2:  # запуск 2 режима
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
-elif mode == 3:
+  print("Decrypted: " + answer2(text, rotation))
+elif mode == 3:  # запуск 3 режима
   print("Bruteforcing...")
   print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
+
